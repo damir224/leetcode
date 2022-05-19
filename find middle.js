21c248819a6e11cfb928d3c18162c566cfc55ee3
yad <damir224@bk.ru>
2022-05-20 01:47:00 +0500

    find midlle

---
+++ find middle.js
@@ -0,0 +1,12 @@
+const l = 7;
+const r = 30;
+// 30-7 = 23 / 2 = 11.5
+const m = Math.floor((r - l) / 2); // 11
+const m1 = Math.floor((r + l) / 2); // 18
+const m2 = l + Math.floor((r - l) / 2); // 18
+const m3 = l + r >> 1; // 18
+
+console.log('m', m)
+console.log('m1', m1)
+console.log('m2', m2)
+console.log('m3', m3)
