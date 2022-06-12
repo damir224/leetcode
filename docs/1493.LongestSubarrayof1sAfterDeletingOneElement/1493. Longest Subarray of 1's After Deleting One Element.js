c76c314501ede3e48e3ff1bff876e31c00b8baf6
yad <damir224@bk.ru>
2022-06-12 11:44:58 +0500

    1493

--- docs/1493.LongestSubarrayof1sAfterDeletingOneElement/1493. Longest Subarray of 1's After Deleting One Element.js
+++ docs/1493.LongestSubarrayof1sAfterDeletingOneElement/1493. Longest Subarray of 1's After Deleting One Element.js
@@ -1,59 +1,37 @@
 const longestSubarray = function (nums) {
-  let start = 0,
-    end = 0,
-    count_zeros = 0,
-    max_len = 0;
-
-  while (end <= (nums.length - 1)) {
-    if (nums[end] == 0) {
-      count_zeros += 1;
+  let firstZero = null,
+    windowEnd = 0,
+    windowSum = 0;
+  while (windowEnd < nums.length) {
+    if (nums[windowEnd] === 0 &&  firstZero !== null) {
+      
+      windowSum = Math.max(windowSum, windowEnd - windowStart);
+      windowStart = windowEnd + 1;
     }
-    while (count_zeros > 1) {
-      if (nums[start] == 0) {
-        count_zeros -= 1;
-      }
-      start += 1;
 
+    if(nums[windowEnd] === 0 && firstZero === null) {
+      firstZero = windowEnd;
     }
-    max_len = Math.max(max_len, end - start);
-    end += 1;
+    windowEnd++;
   }
-  return max_len;
+  return Math.max(windowSum, windowEnd - windowStart);
 };
 
-// const longestSubarray = function (nums) {
-//   let start = 0;
-//   (end = 0), (res = 0), (count = 1), (endPrev = 0);
-//   for (end; end < nums.length; end++) {
-//     if (nums[end] === 1) {
-//       break;
-//     }
-//   }
-//   endPrev = start = end;
-//   for (end; end < nums.length; end++) {
-//     res = Math.max(res, end - start);
-//     if (nums[end] === 0 && nums[end+1] !== 0) {
-//       if (count === 0) {
-//         start = endPrev+1;
-//         count = 1;
-//       } else if (count === 1) {
-//         start++;
-//         endPrev = end + 1;
-//         count--;
-//       }
-//     } else  if (nums[end] === 0 && nums[end+1] === 0){
-//       res = Math.max(res, end - start);
-//       endPrev = start = end+1;
-//       count = 1
-//     }
-//   }
-//   res = Math.max(res, end - start);
-//   return nums.length === res ? res - 1 : res;
-// };
+console.log('3 === ', longestSubarray([1, 1, 0, 1])); // 3
+console.log('5 === ', longestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1])); // 5
+console.log('2 === ', longestSubarray([1, 1, 1])); // 2
+console.log('4 === ', longestSubarray([1,1,0,1,0,1,1,1,0,1])); // 4
 
-// console.log('answer is 3', longestSubarray([1, 1, 0, 1])); // 3
-// console.log('answer is 5', longestSubarray([0, 1, 1, 1, 0, 1, 1, 0, 1, 0])); // 5
-console.log('answer is 2', longestSubarray([1, 1, 1])); // 2
-console.log('answer is 4', longestSubarray([1, 1, 0, 0, 1, 1, 1, 0, 1])); // 4
-// console.log('answer is 2', longestSubarray([0, 0, 1, 1])); // 2
-// console.log('answer is 18',longestSubarray([1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,])); // 18
+// const longestSubarray = function (nums) {
+//   let windowStart = 0,
+//     windowEnd = 0,
+//     windowSum = 0;
+//   while (windowEnd < nums.length) {
+//     if (nums[windowEnd] === 0) {
+//       windowSum = Math.max(windowSum, windowEnd - windowStart);
+//       windowStart = windowEnd + 1;
+//     }
+//     windowEnd++;
+//   }
+//   return Math.max(windowSum, windowEnd - windowStart);
+// };
--- docs/1493.LongestSubarrayof1sAfterDeletingOneElement/README.md
+++ docs/1493.LongestSubarrayof1sAfterDeletingOneElement/README.md
@@ -6,6 +6,57 @@ https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
 
 ```js
 const longestSubarray = function (nums) {
+  let windowStart = 0,
+    windowEnd = 0,
+    windowSum = 0,
+    firstZeroIndex = -1;
+  while (windowEnd < nums.length) {
+    if (firstZeroIndex !== null && nums[windowEnd] === 0) {
+      windowSum = Math.max(windowSum, windowEnd - windowStart);
+      windowStart = firstZeroIndex;
+      firstZeroIndex = windowEnd + 1;
+    }
+    if (firstZeroIndex === null && nums[windowEnd] === 0) {
+      firstZeroIndex = windowEnd + 1;
+    } 
+    windowEnd++;
+  }
+
+  return Math.max(windowSum, windowEnd - windowStart) - 1;
+};
+```
+---
+
+```js
+const longestSubarray = function (nums) {
+  let windowStart = 0,
+    windowEnd = 0,
+    windowSum = 0,
+    zeroSum = 0,
+    firstZero = 0;
+  while (windowEnd < nums.length) {
+    if (zeroSum === 0 && nums[windowEnd] === 0) {
+      firstZero = windowEnd + 1;
+      zeroSum++;
+    } else if (zeroSum === 1 && nums[windowEnd] === 0) {
+      zeroSum++;
+    }
+    if (zeroSum === 2) {
+      windowSum = Math.max(windowSum, windowEnd - windowStart);
+      windowStart = firstZero;
+      firstZero = windowEnd + 1;
+      zeroSum--;
+    }
+    windowEnd++;
+  }
+
+  return Math.max(windowSum, windowEnd - windowStart) - 1;
+};
+```
+---
+
+```js
+const longestSubarray = function (nums) {
   let start = 0,
     end = 0,
     count_zeros = 0,
